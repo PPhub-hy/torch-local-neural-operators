e03bcc83f37419b75be3211f903e47e54b7fc054
PPhub-hy <59812286+PPhub-hy@users.noreply.github.com>
2023-12-04 12:10:35 +0800

    Add files via upload

--- Train_Validation/main.py
+++ Train_Validation/main.py
@@ -1,5 +1,4 @@
 from lib.networks_LNO import NetNS_InN_legendre, NetWave_InN_legendre, NetBurgers1D_InN_legendre
-from lib.networks_FNO import NetNS_InN_fourior_L, NetWave_InN_fourior_L, NetBurgers1D_InN_fourior_L
 
 from Data.DatasetNS import NS_Dataset2D
 from Data.DatasetWave import Wave_Dataset2D
@@ -16,7 +15,7 @@ import math
 import scipy.io as scio
 import numpy as np
 
-PROBLEM ='Wave' #'NS' #'Burgers1D'#'Burgers2D' #'Wave'
+PROBLEM ='NS' #'NS' #'Burgers1D'#'Burgers2D' #'Wave'
 
 if PROBLEM == 'NS' or PROBLEM == 'Burgers2D':
     from lib.train import train_iterative_NS_InN as train
@@ -35,7 +34,7 @@ args = parser.parse_args()
 in_length = None
 # network parameters
 if PROBLEM == 'NS' or PROBLEM == 'Burgers2D':
-    Re = 1000
+    Re = 500
     t_interval = 5
     in_length = 1
 elif PROBLEM == 'Wave':
@@ -65,11 +64,11 @@ def get_parameter_number(network):
 
 def train_test_save():
     if PROBLEM == 'NS':
-        orders_all = [i for i in range(11, 12)] #Re500-mini
-        #orders_all = [i for i in range(11, 111)] #Re1000
-        #orders_all = [i for i in range(11, 136)] #Re500
-        #orders_all = [i for i in range(11, 211)] #Re100
-        #orders_all = [i for i in range(11, 511)] #Re50
+        #orders_all = [i for i in range(1, 12)] #Re500-mini
+        #orders_all = [i for i in range(1, 111)] #Re1000
+        orders_all = [i for i in range(1, 136)] #Re500
+        #orders_all = [i for i in range(1, 211)] #Re100
+        #orders_all = [i for i in range(1, 511)] #Re50
 
         orders_test = [i for i in range(1, 11)]
         orders_train = [order for order in orders_all if order not in orders_test]
@@ -77,51 +76,47 @@ def train_test_save():
         print('orders_train = ', orders_train)
         print('orders_test = ', orders_test)
 
-        dataset = NS_Dataset2D(data_dir='/data/LNOdata/',data_name='NS128Re{}t1000'.format(Re), orders_train=orders_train, orders_test=orders_test,
+        dataset = NS_Dataset2D(data_dir='./Data/',data_name='NS128Re{}t1000'.format(Re), orders_train=orders_train, orders_test=orders_test,
                                t_interval=t_interval)
 
         network = NetNS_InN_legendre(num_blocks=4, In_length=in_length)
         #network = NetNS_InN_legendre(num_blocks=4, In_length=in_length, cheb=True)
-        #network = NetNS_InN_fourior_L(12, 20)
 
     elif PROBLEM == 'Wave':
         orders_all = [i for i in range(11, 211)]
         orders_test = [i for i in range(1, 11)]
         orders_train = [order for order in orders_all if order not in orders_test]
 
-        dataset = Wave_Dataset2D(data_dir='/data/LNOdata/', data_name='Wave128t1000', orders_train=orders_train,
+        dataset = Wave_Dataset2D(data_dir='./Data/', data_name='Wave128t1000', orders_train=orders_train,
                                orders_test=orders_test,
                                t_interval=t_interval)
 
-        #network = NetWave_InN_legendre(num_blocks=4)
+        network = NetWave_InN_legendre(num_blocks=4)
         #network = NetWave_InN_legendre(num_blocks=4, cheb=True)
-        network = NetWave_InN_fourior_L(12, 20)
 
     elif PROBLEM =='Burgers2D':
         orders_all = [i for i in range(11, 211)]
         orders_test = [i for i in range(1, 11)]
         orders_train = [order for order in orders_all if order not in orders_test]
 
-        dataset = Burgers_Dataset2D(data_dir='/data/LNOdata/', data_name='Burgers2D128Re100t1000', orders_train=orders_train,
+        dataset = Burgers_Dataset2D(data_dir='./Data/', data_name='Burgers2D128Re100t1000', orders_train=orders_train,
                                  orders_test=orders_test,
                                  t_interval=t_interval)
 
-        #network = NetNS_InN_legendre(num_blocks=4, In_length=in_length)
+        network = NetNS_InN_legendre(num_blocks=4, In_length=in_length)
         #network = NetNS_InN_legendre(num_blocks=4, In_length=in_length, cheb=True)
-        network = NetNS_InN_fourior_L(12, 20)
 
     elif PROBLEM == 'Burgers1D':
         orders_all = [i for i in range(11, 211)]
         orders_test = [i for i in range(1, 11)]
         orders_train = [order for order in orders_all if order not in orders_test]
 
-        dataset = Burgers_Dataset(data_dir='/data/LNOdata/', data_name='Burgers128Re100t1000', orders_train=orders_train,
+        dataset = Burgers_Dataset(data_dir='./Data/', data_name='Burgers128Re100t1000', orders_train=orders_train,
                                  orders_test=orders_test,
                                  t_interval=t_interval)
 
         network = NetBurgers1D_InN_legendre(num_blocks=4, In_length=1)
         #network = NetBurgers1D_InN_legendre(num_blocks=4, In_length=1, cheb=True)
-        #network = NetBurgers1D_InN_fourior_L(12,20)
 
     print(dataset.cache_names)
     print(dataset.sample_nums)
@@ -130,8 +125,6 @@ def train_test_save():
     print(get_parameter_number(network))
     network = network.cuda()
     optimizer = optim.Adam(network.parameters(), lr=learning_rate, weight_decay=weight_decay)
-    #optimizer = optim.SGD(network.parameters(), lr=0.005, momentum=momentum,
-    #                             weight_decay=weight_decay)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.7, last_epoch=-1)
 
     recurrent = 10
@@ -162,7 +155,7 @@ def load_test(out_name):
         orders_test = [i for i in range(1, 11)]
         ground_truth_name = 'NS128Re{}t1000'.format(Re)
 
-        dataset = NS_Dataset2D(data_dir='/data/LNOdata/', data_name='NS128Re{}t1000'.format(Re), orders_train=[],
+        dataset = NS_Dataset2D(data_dir='./Data/', data_name='NS128Re{}t1000'.format(Re), orders_train=[],
                                orders_test=orders_test,
                                t_interval=t_interval)
 
@@ -170,7 +163,7 @@ def load_test(out_name):
         orders_test = [i for i in range(1, 11)]
         ground_truth_name = 'Wave128t1000'
 
-        dataset = Wave_Dataset2D(data_dir='/data/LNOdata/', data_name='Wave128t1000', orders_train=[],
+        dataset = Wave_Dataset2D(data_dir='./Data/', data_name='Wave128t1000', orders_train=[],
                                orders_test=orders_test,
                                t_interval=t_interval)
 
@@ -178,7 +171,7 @@ def load_test(out_name):
         orders_test = [i for i in range(1, 11)]
         ground_truth_name = 'Burgers2D128Re100t1000'
 
-        dataset = Burgers_Dataset2D(data_dir='/data/LNOdata/', data_name='Burgers2D128Re100t1000'.format(Re),
+        dataset = Burgers_Dataset2D(data_dir='./Data/', data_name='Burgers2D128Re100t1000'.format(Re),
                                orders_train=[], orders_test=orders_test,
                                t_interval=t_interval)
 
@@ -186,7 +179,7 @@ def load_test(out_name):
         orders_test = [i for i in range(1, 11)]
         ground_truth_name = 'Burgers128Re100t1000'
 
-        dataset = Burgers_Dataset(data_dir='/data/LNOdata/', data_name='Burgers128Re100t1000', orders_train=[],
+        dataset = Burgers_Dataset(data_dir='./Data/', data_name='Burgers128Re100t1000', orders_train=[],
                                  orders_test=orders_test,
                                  t_interval=t_interval)
 
@@ -205,7 +198,7 @@ def MeanSquareError(out_name, ground_truth_name, orders_test):
     MSE = np.zeros(len(test_t))
     for k in range(len(orders_test)):
         order = orders_test[k]
-        ground_truth = scio.loadmat('/data/LNOdata/' + ground_truth_name + '/' + ground_truth_name + '_' + str(order) + '.mat')
+        ground_truth = scio.loadmat('./Data/' + ground_truth_name + '/' + ground_truth_name + '_' + str(order) + '.mat')
         i = 0
         for ii in test_t:
             ground_truth_u = ground_truth['u'][(ii+1) * t_interval, :].reshape(NG, NG)
@@ -215,86 +208,6 @@ def MeanSquareError(out_name, ground_truth_name, orders_test):
     MSE = MSE/len(orders_test)
     print('MSE(t=0.5, 1, 2, 5)={}'.format(MSE))
 
-def load_test_new_sample():
-    with torch.no_grad():
-        print(PROBLEM)
-        if PROBLEM == 'NS':
-            sample_dir = '/data/LNOdata/NS128Re{}t1000'.format(Re) + '/NS128Re{}t1000'.format(Re) + '_test1.mat'
-            if Re == 100:
-                model_file = 'InComNS_Re100_200ep_LNO-Legendre_n12m6k2_Adam0.001+step_realLoss_interval50_baseline_NormSigma_test3_model.pp'
-            if Re == 500:
-                model_file = 'InComNS_Re500_200ep_LNO-Legendre_n16m8k2_Adam0.001+step_realLoss_interval50_baseline_NormSigma_test6_model.pp'
-            if Re == 1000:
-                model_file = 'InComNS_Re1000_200ep_LNO-Legendre_n24m8k2_Adam0.001+step_realLoss_interval50_baseline_NormSigma_test8_model.pp'
-        elif PROBLEM == 'Wave':
-            sample_dir = '/data/LNOdata/Wave128t1000/Wave128t1000_test1.mat'
-            model_file = 'Wave_200ep_LNO-Legendre_n12m4k2_interval100_recheck_test3_model.pp'
-        elif PROBLEM == 'Burgers2D':
-            sample_dir = '/data/LNOdata/Burgers2D128Re100t1000/Burgers2D128Re100t1000_test1.mat'
-            model_file = 'Burgers2D_200ep_LNO-Legendre_n12m6k2_Adam0.001+step_realLoss_interval50_test3_model.pp'
-        elif PROBLEM == 'Burgers1D':
-            sample_dir = '/data/LNOdata/Burgers128Re100t1000/Burgers362_test4.mat'
-            model_file = 'Burgers1D_200ep_LNO-Legendre_n12m6k2_Adam0.001+step_realLoss_interval50_test3_model.pp'
-
-
-        network = torch.load('models/' + model_file)
-
-        long_round = 100
-        record_round = 1
-        data = scio.loadmat(sample_dir)
-        if PROBLEM == 'Burgers1D':
-            Length_x = 362
-
-            inp = np.zeros((1, 1 * in_length, Length_x), np.float32)
-            for ii in range(in_length):
-                inp[0, 0 + ii * 1, :] = data['u'][t_interval * ii, :-1]
-            inp = torch.from_numpy(inp).cuda()
-
-            data_matlab = torch.zeros(long_round // record_round, inp.shape[0], 1, Length_x)
-            for i in range(long_round):
-                output = network(inp)
-                if i % record_round == 0:
-                    data_matlab[i // record_round, :, :, :] = output[:, :, :]
-                new_in = torch.zeros(inp.shape[0], 1 * in_length, Length_x).cuda()
-                home = 1 * (in_length - 1)
-                new_in[:, :home, :] = inp[:, 1:, :]
-                new_in[:, home:home + 1, :] = output
-                inp = new_in
-
-        else:
-            Length_y = 181
-            Length_x = 128
-
-            inp = np.zeros((1, 2 * in_length, Length_y, Length_x), np.float32)
-            if PROBLEM == 'NS' or PROBLEM == 'Burgers2D':
-                for ii in range(in_length):
-                    inp[0, 0 + ii * 2, :, :] = data['u'][t_interval * ii, :].reshape((Length_y, Length_x))
-                    inp[0, 1 + ii * 2, :, :] = data['v'][t_interval * ii, :].reshape((Length_y, Length_x))
-            elif PROBLEM == 'Wave':
-                for ii in range(in_length):
-                    inp[0, 0 + ii * 2, :, :] = data['p'][t_interval * ii, :].reshape((Length_y, Length_x))
-                    inp[0, 1 + ii * 2, :, :] = data['dp'][t_interval * ii, :].reshape((Length_y, Length_x))
-            inp = torch.from_numpy(inp).cuda()
-
-            data_matlab = torch.zeros(long_round // record_round, inp.shape[0], 2, Length_y, Length_x)
-            for ii in range(long_round):
-                output = network(inp)
-                if ii % record_round == 0:
-                    data_matlab[ii // record_round, :, :, :, :] = output[:, :, :, :]
-                new_in = torch.zeros(inp.shape[0], 2 * in_length, Length_y, Length_x).cuda()
-                home = 2 * (in_length - 1)
-                new_in[:, :home, :, :] = inp[:, 2:, :, :]
-                new_in[:, home:home + 2, :, :] = output
-                inp = new_in
-
-        if PROBLEM == 'NS':
-            scio.savemat('outputs/NS128Re{}t1000'.format(Re) + '_output_test1.mat', mdict={"output": data_matlab.detach().numpy()})
-        elif PROBLEM == 'Wave':
-            scio.savemat('outputs/Wave128t1000_output_recheck_test1.mat', mdict={"output": data_matlab.detach().numpy()})
-        elif PROBLEM == 'Burgers2D':
-            scio.savemat('outputs/Burgers2D128Re100t1000_output_test1.mat', mdict={"output": data_matlab.detach().numpy()})
-        elif PROBLEM == 'Burgers1D':
-            scio.savemat('outputs/Burgers128Re100t1000_output_test4.mat', mdict={"output": data_matlab.detach().numpy()})
 
 
 if __name__ == '__main__':
